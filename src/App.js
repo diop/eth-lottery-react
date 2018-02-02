7b6277c8afe670c37c31ad736139773814862dff
diop <fodesdiop@gmail.com>
2018-02-01 16:48:27 -0800

    Render web3 address

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3'
+import lottery from './lottery'
 
 class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call()
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
+
+    this.setState({ manager, players, balance })
+  }
+
   render() {
-    web3.eth.getAccounts()
-      .then(console.log)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Eth Lottery</h1>
-        </header>
-        <p className="App-intro">
-          Are you ready to roll the diiice?
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by: { this.state.manager }. 
+          There are currently { this.state.players.length } people entered,
+          competing to win { web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+
+        <hr />
+
+        <form>
+        <h4>Feeling lucky?</h4>
+          <div>
+          <label>Amount of ether to enter</label>
+          <input
+          value={this.state.value}
+          onChange={event => this.setState({ value: event.target.value }) }
+          />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
